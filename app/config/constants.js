6b38a9c01321f8417667332e2bb325d7ababcbf0
DeltaV3 <cristiand1346@gmail.com>
2025-01-10 17:06:21 -0500

    Links updated

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,6 +1,6 @@
 const APP_DATA_NAME = 'ZLauncher'
-const DISTRIBUTION_URL = 'https://techterra.x10.mx/nebula/distribution.json'
-const DISTRIBUTION_META_URL = 'https://techterra.x10.mx/nebula/distrometa.json'
+const DISTRIBUTION_URL = 'https://ZLauncher.es/nebula/distribution.json'
+const DISTRIBUTION_META_URL = 'https://ZLauncher.es/nebula/distrometa.json'
 
 const DEFAULT_DISTRIBUTION_META = {}
 const DEFAULT_SERVER_META = { allowOptIn: true }
